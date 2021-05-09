9cd555d2b3797db930982862525c19bb8e201989
SeungGumon <b9327912@gmail.com>
2021-05-09 18:50:38 +0900

    Finish Login Page

---
+++ src/components/button.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+interface IButtonProps {
+    canClick: boolean;
+    loading: boolean;
+    actionText: string;
+}
+
+
+export const Button: React.FC<IButtonProps> =
+    ({
+         canClick,
+         loading,
+         actionText
+     }) =>
+        <button
+            className={`text-lg font-medium text-white py-4 focus:outline:none transition-colors ${canClick ? "bg-lime-600 hover:bg-lime-700" : "bg-gray-300 pointer-events-none"}`}>
+            {loading ? "Loading..." : actionText}
+        </button>
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import {useForm} from "react-hook-form";
 import {FormError} from "../components/form-error";
 import {gql, useMutation} from "@apollo/client";
+import nuberLogo from "../images/logo.svg"
 import {
     loginMutation,
     loginMutationVariables,
 } from "../__generated__/loginMutation";
+import {Button} from "../components/button";
+import {Link} from 'react-router-dom'
 
 
 interface ILoginForm {
@@ -26,7 +29,7 @@ const LOGIN_MUTATION = gql`
 
 export const Login = () => {
 
-    const {register, getValues, errors, handleSubmit} = useForm<ILoginForm>();
+    const {register, getValues, errors, handleSubmit, formState} = useForm<ILoginForm>({mode: "onChange"});
 
     const onCompleted = (data: loginMutation) => {
         const {
@@ -61,10 +64,11 @@ export const Login = () => {
     }
 
     return (
-        <div className='h-screen flex items-center justify-center bg-gray-800'>
-            <div className='bg-white w-full max-w-lg pt-10 pb-5 py-10 rounded-lg text-center'>
-                <h3 className='text-3xl text-gray-800'>Log In</h3>
-                <form onSubmit={handleSubmit(onSubmit)} className='grid gap-3 mt-5 px-5 '>
+        <div className='h-screen flex items-center flex-col mt-10 lg:mt-28'>
+            <div className={'w-full max-w-screen-sm flex flex-col px-5 items-center'}>
+                <img src={nuberLogo} alt={"Logo"} className='w-52 mb-5'/>
+                <h4 className="w-full font-medium text-left text-3xl mb-7">Welcome Back</h4>
+                <form onSubmit={handleSubmit(onSubmit)} className='grid gap-3 w-full mb-3'>
                     <input
                         ref={register({
                             required: "이메일은 필수 사항 입니다."
@@ -95,10 +99,15 @@ export const Login = () => {
                     {errors.password?.type === "minLength" && (
                         <FormError errorMessage={'비밀번호는 최소 10글자 적어주세요'}/>
                     )}
-                    <button className={'mt-5 btn'} onClick={onSubmit}>{loginLoading ? "Loading..." : "Log In"}</button>
+                    <Button canClick={formState.isValid} loading={loginLoading} actionText={"Log In"}/>
                     {loginMutationResult?.login.error && <FormError errorMessage={loginMutationResult.login.error}/>}
                 </form>
+                <div>
+                    New to Nuber ? <Link to={'/create-account'} className={'text-lime-600 hover:underline'}>Create an
+                    Account</Link>
+                </div>
             </div>
         </div>
+
     )
 }
