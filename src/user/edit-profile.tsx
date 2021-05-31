f0f56784c33837fd4d2e009cf76329691d285c0c
SeungGumon <b9327912@gmail.com>
2021-06-01 00:41:53 +0900

    creating Edit Profile

--- src/user/edit-profile.tsx
+++ src/user/edit-profile.tsx
@@ -2,37 +2,73 @@ import React from 'react';
 import {useMe} from "../hooks/useMe";
 import {Button} from "../components/button";
 import {gql, useApolloClient, useMutation} from "@apollo/client";
+import {useForm} from "react-hook-form";
+import {editProfile, editProfileVariables} from "../__generated__/editProfile";
 
 
+const EDIT_PROFILE_MUTATION = gql`
+    mutation editProfile($input: EditProfileInput!) {
+        editProfile(input : $input) {
+            ok
+            error
+        }
+    }
+`
 
-interface IEditProfile {
-    email : string
-    password : string
+
+interface IFormProps {
+    email?: string;
+    password?: string;
 }
 
 
 export const EditProfile = () => {
 
 
-
     const {data: userData} = useMe();
 
-    const EDIT_PROFILE_MUTATION = gql`
-        mutation editProfile($input : EditProfileInput!) {
-            editProfile(input : $input) {
-                ok
-                error
-            }
+    const onCompleted = (data: editProfile) => {
+        const {editProfile: {error, ok}} = data;
+        if (ok) {
+            //update the Cache
         }
-    `
+    }
+
+
+    const [editProfile, {loading: editLoading}] = useMutation<editProfile, editProfileVariables>(EDIT_PROFILE_MUTATION, {
+        onCompleted
+    })
+
+    const {register, handleSubmit, getValues, formState} = useForm<IFormProps>({
+        mode: "onChange",
+        defaultValues: {
+            email: userData?.me?.email,
+        }
+    });
+
+    const onSubmit = () => {
+        const {email, password} = getValues();
+        editProfile({
+            variables: {
+                input: {
+                    email,
+                    ...(password !== "" && {password})
+                }
+            }
+        })
+    }
+
 
     return (
         <div className="mt-52 flex flex-col justify-center items-center">
             <h4 className={"font-semibold text-2xl mb-3"}>Edit Profile</h4>
-            <form className={"grid max-w-screen-sm gap-3 mt-5 w-full mb-5"}>
-                <input className={'input'} type={"email"} placeholder={'Email'} required={true}/>
-                <input className={'input'} type={"password"} placeholder={'password'}/>
-                <Button canClick={true} loading={false} actionText={"Save Profile"}/>
+            <form onSubmit={handleSubmit(onSubmit)} className={"grid max-w-screen-sm gap-3 mt-5 w-full mb-5"}>
+                <input ref={register({
+                    pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                })} name={'email'} className={'input'} type={"email"} placeholder={'Email'}
+                       required={true}/>
+                <input ref={register} name={'password'} className={'input'} type={"password"} placeholder={'password'}/>
+                <Button canClick={formState.isValid} loading={editLoading} actionText={"Save Profile"}/>
             </form>
         </div>
     )
